0d2cf0fda5f398b069e4f1860ec09202a7bffba1
David Siegel <djsiegel@gmail.com>
2014-06-12 10:03:30 -0700

    Clarify names

--- Shallow/SwipeGestureHandler.fs
+++ Shallow/SwipeGestureHandler.fs
@@ -22,29 +22,28 @@ type SwipeGestureHandler(referenceView: UIView, targetView: UIView) =
         atan2 view.Transform.yx view.Transform.xx
 
     let handlePan (gesture: UIPanGestureRecognizer) =
-        let view, superview = gesture.View, gesture.View.Superview
         match gesture.State, attachment with
         | UIGestureRecognizerState.Began, _ ->
             animator.RemoveAllBehaviors()
-            startCenter <- view.Center
+            startCenter <- targetView.Center
 
             let offset =
-                let size = view.Bounds.Size
+                let size = targetView.Bounds.Size
                 // calculate the center offset and anchor point
-                let pointWithinAnimatedView = gesture.LocationInView(view)
+                let pointWithinAnimatedView = gesture.LocationInView(targetView)
                 UIOffset(pointWithinAnimatedView.X - size.Width / 2.0f, pointWithinAnimatedView.Y- size.Height / 2.0f)
-            let anchor = gesture.LocationInView(superview)
+            let anchor = gesture.LocationInView(referenceView)
 
             // calculate angular velocity
             lastTime <- DateTime.Now
-            lastAngle <- angleOfView view
+            lastAngle <- angleOfView targetView
 
             // create attachment behavior
             attachment <-
-                let attach = UIAttachmentBehavior(view, offset, anchor)
+                let attach = UIAttachmentBehavior(targetView, offset, anchor)
                 attach.Action <- fun () ->
                     let time = DateTime.Now
-                    let angle = angleOfView view
+                    let angle = angleOfView targetView
                     if time > lastTime then
                         let seconds = float32 (time - lastTime).TotalSeconds
                         angularVelocity <- (angle - lastAngle) / seconds
@@ -55,33 +54,33 @@ type SwipeGestureHandler(referenceView: UIView, targetView: UIView) =
 
         | UIGestureRecognizerState.Changed, Some attachment ->
             // as user makes gesture, update attachment behavior's anchor point, achieving drag 'n' rotate
-            attachment.AnchorPoint <- gesture.LocationInView(superview)
+            attachment.AnchorPoint <- gesture.LocationInView(referenceView)
 
         | UIGestureRecognizerState.Ended, _ ->
             animator.RemoveBehaviors()
-            let velocity = gesture.VelocityInView(superview)
+            let velocity = gesture.VelocityInView(referenceView)
 
             // if we aren't dragging it down, just snap it back and quit
             let pi = float32 Math.PI
             if abs ((atan2 velocity.Y velocity.X) - pi / 2.0f) > pi / 4.0f then
-                animator.AddBehavior(UISnapBehavior(view, startCenter))
+                animator.AddBehavior(UISnapBehavior(targetView, startCenter))
 
             // otherwise, create UIDynamicItemBehavior that carries on animation from where
             // the gesture left off (notably linear and angular velocity)
             else
-                let dynamic = UIDynamicItemBehavior(view, AngularResistance = 2.0f)
-                dynamic.AddLinearVelocityForItem(velocity, view)
-                dynamic.AddAngularVelocityForItem(angularVelocity, view)
+                let dynamic = UIDynamicItemBehavior(targetView, AngularResistance = 2.0f)
+                dynamic.AddLinearVelocityForItem(velocity, targetView)
+                dynamic.AddAngularVelocityForItem(angularVelocity, targetView)
 
                 // when the view no longer intersects with its superview, go ahead and remove it
                 dynamic.Action <- fun () ->
-                    if not (superview.Bounds.IntersectsWith(view.Frame)) then
+                    if not (referenceView.Bounds.IntersectsWith(targetView.Frame)) then
                         animator.RemoveAllBehaviors()
-                        view.RemoveFromSuperview()
+                        targetView.RemoveFromSuperview()
                 
                 animator.AddBehavior(dynamic)
                 // add a little gravity so it accelerates off the screen (in case user gesture was slow)
-                animator.AddBehavior(UIGravityBehavior(view, Magnitude = 0.7f))
+                animator.AddBehavior(UIGravityBehavior(targetView, Magnitude = 0.7f))
 
     do targetView.AddGestureRecognizer(UIPanGestureRecognizer(handlePan))
 
