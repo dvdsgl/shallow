c1ed7913797fceead3bd0783066eea72fe537073
David Siegel <djsiegel@gmail.com>
2014-06-12 10:31:29 -0700

    Cleanup

--- Shallow/AppDelegate.fs
+++ Shallow/AppDelegate.fs
@@ -5,20 +5,19 @@ open MonoTouch.UIKit
 open MonoTouch.Foundation
 
 [<Register ("AppDelegate")>]
-type AppDelegate () =
-    inherit UIApplicationDelegate ()
+type AppDelegate() =
+    inherit UIApplicationDelegate()
 
-    let window = new UIWindow (UIScreen.MainScreen.Bounds)
+    let window = UIWindow(UIScreen.MainScreen.Bounds)
 
-    // This method is invoked when the application is ready to run.
-    override this.FinishedLaunching (app, options) =
-        window.RootViewController <- new ShallowViewController ()
-        window.MakeKeyAndVisible ()
+    override this.FinishedLaunching(app, options) =
+        window.RootViewController <- ShallowViewController()
+        window.MakeKeyAndVisible()
         true
 
 module Main =
     [<EntryPoint>]
     let main args =
-        UIApplication.Main (args, null, "AppDelegate")
+        UIApplication.Main(args, null, "AppDelegate")
         0
 
