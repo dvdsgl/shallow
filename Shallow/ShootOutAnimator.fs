32c3d98d20774e5d4babe846bf1299a6f4390931
David Siegel <djsiegel@gmail.com>
2014-06-13 09:42:40 -0700

    No need to use PointF

--- Shallow/ShootOutAnimator.fs
+++ Shallow/ShootOutAnimator.fs
@@ -10,10 +10,10 @@ type Direction =
 
     member this.UnitVector =
         match this with
-        | North -> PointF(0.0f, 1.0f)
-        | South -> PointF(0.0f, -1.0f)
-        | West -> PointF(-1.0f, 0.0f)
-        | East -> PointF(1.0f, 0.0f)
+        | North -> (0.0f, 1.0f)
+        | South -> (0.0f, -1.0f)
+        | West -> (-1.0f, 0.0f)
+        | East -> (1.0f, 0.0f)
 
 type ShootOutAnimator(referenceView: UIView) =
     let speed = 1300.0f
@@ -23,8 +23,8 @@ type ShootOutAnimator(referenceView: UIView) =
     member this.ViewShotOut = shotOut.Publish
 
     member this.ShootOut(view: UIView, direction: Direction) =
+        let x, y = direction.UnitVector
         let dynamic = UIDynamicItemBehavior(view, AngularResistance = 2.0f)
-        let x, y = direction.UnitVector.X, direction.UnitVector.Y
        
         dynamic.AddLinearVelocityForItem(PointF(x * speed, y * speed), view)
         dynamic.AddAngularVelocityForItem(x * 3.0f, view)
@@ -34,4 +34,3 @@ type ShootOutAnimator(referenceView: UIView) =
                 view.RemoveFromSuperview()
                 shotOut.Trigger(view)
         animator.AddBehavior(dynamic)
-        ()
