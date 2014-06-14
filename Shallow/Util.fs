e2a40adcad8e40b14b33039f3264f64b80e8ea34
David Siegel <djsiegel@gmail.com>
2014-06-14 10:52:07 -0700

    Currying behavior is simpler than I thought it was.

--- Shallow/Util.fs
+++ Shallow/Util.fs
@@ -16,11 +16,12 @@ type Dictionary<'Key, 'Value> with
         | true, v -> Some v
         | _ -> None
 
-let memoize f x =
+let memoize f =
     let cache = Dictionary<_,_>(HashIdentity.Structural)
-    match cache.MaybeGet(x) with
-    | Some y -> y
-    | None ->
-        let y = f x
-        cache.[x] <- y
-        y
+    fun x ->
+        match cache.MaybeGet(x) with
+        | Some y -> y
+        | None ->
+            let y = f x
+            cache.[x] <- y
+            y
